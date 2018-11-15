f66eea34bb5cc6766fc496266c498c10c7d3ea36
zoe pot <pot.zoe.s@gmail.com>
2018-11-15 15:26:22 +0000

    program works for fizz, buzz and fizzbuzz + spec tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,18 @@
 def fizzbuzz(number)
-  'fizz'
+  if (number % 3 == 0 && number % 5 != 0)
+    "fizz"
+
+  elsif (number % 5 == 0 && number % 3 != 0)
+    "buzz"
+
+  elsif (number % 3 == 0 && number % 5 == 0)
+    "fizzbuzz"
+
+  else
+    number
+  end
 end
+
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(7)
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,10 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
 end
